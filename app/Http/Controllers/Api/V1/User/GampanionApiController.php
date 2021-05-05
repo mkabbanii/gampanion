f4653ae05fa148795bed047b802a6c4a186d90b7
Mohammed Moussa <binmosa@gmail.com>
2021-05-05 06:13:49 +0300

    changed uSER to User in api/v1/User/GampanionApiController

--- app/Http/Controllers/Api/V1/User/GampanionApiController.php
+++ app/Http/Controllers/Api/V1/User/GampanionApiController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1\uSER;
+namespace App\Http\Controllers\Api\V1\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Traits\MediaUploadingTrait;
