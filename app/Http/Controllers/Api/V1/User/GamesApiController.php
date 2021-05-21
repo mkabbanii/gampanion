45ed548ed2b09529d6372db5c8617822d94b55b3
hazem-en <62343331+hazem-en@users.noreply.github.com>
2021-05-21 22:23:54 +0300

    Fixing Games
    adding tow new fields to requset:
    add_by "Number Of Gampanion Added This Game To Their Games"
    gampanion_data "The Gampanion Object Who Have Added This Game To Their Games To Be Shown In Games Page"

--- app/Http/Controllers/Api/V1/User/GamesApiController.php
+++ app/Http/Controllers/Api/V1/User/GamesApiController.php
@@ -7,12 +7,13 @@ use App\Http\Requests\StoreGameRequest;
 use App\Http\Requests\UpdateGameRequest;
 use App\Http\Resources\Admin\GameResource;
 use App\Models\Game;
+use App\Models\User;
 use Gate;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
-use App\User;
+//use App\User;
 
 class GamesApiController extends Controller
 {
@@ -20,16 +21,27 @@ class GamesApiController extends Controller
 
     public function index()
     {
-       return new GameResource(Game::all());        
+        $games = Game::all();
+        foreach ($games as $game) {
+            $gampanion_data = array();
+            $gampanions = $game->gameGampanions()->get();
+            $game->add_by = $gampanions->count();
+
+            foreach ($gampanions as $gampanion) {
+                $gampanion_data[] =User::find($gampanion->user_id);
+            }
+            $game->gampanion_data = $gampanion_data;
+        }
+        return new GameResource($games);
     }
 
     public function AllAndGampanions()
     {
         return new GameResource(Game::with('gameGampanions')->get());
     }
-    
+
     public function show(Game $game)
     {
-        return Game::with('gameGampanions')->where('id',$game->id)->get();
+        return Game::with('gameGampanions')->where('id', $game->id)->get();
     }
 }
